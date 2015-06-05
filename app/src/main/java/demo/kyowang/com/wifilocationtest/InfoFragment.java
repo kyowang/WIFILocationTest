2b38359a0802e88fb985d07f3d5b42e0c111f5da
kyowang <wk0328@163.com>
2015-06-05 18:15:19 +0800

    baidu location

--- app/src/main/java/demo/kyowang/com/wifilocationtest/InfoFragment.java
+++ app/src/main/java/demo/kyowang/com/wifilocationtest/InfoFragment.java
@@ -15,6 +15,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+
 import java.util.TimerTask;
 
 
@@ -31,6 +36,11 @@ public class InfoFragment extends android.support.v4.app.Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_WIFI_INFO = "wifi_info";
     private static final String ARG_MOBILE_INFO = "mobile_info";
+
+
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
+    LocationClientOption option = new LocationClientOption();
 
     // TODO: Rename and change types of parameters
     private WifiInfo mWifiInfo;
@@ -99,6 +109,8 @@ public class InfoFragment extends android.support.v4.app.Fragment {
             mWifiInfo = (WifiInfo)getArguments().getParcelable(ARG_WIFI_INFO);
             mParam2 = getArguments().getString(ARG_MOBILE_INFO);
         }
+        mLocationClient = new LocationClient(getActivity().getApplicationContext());
+        mLocationClient.registerLocationListener( myListener );
     }
 
     @Override
@@ -137,18 +149,31 @@ public class InfoFragment extends android.support.v4.app.Fragment {
         updateWifiInfo(mWifiInfo);
 
     }
+    public void setOption()
+    {
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setCoorType("bd09ll");
+        option.setScanSpan(5000);
+        option.setIsNeedAddress(true);
+        option.setNeedDeviceDirect(false);
+    }
 
     @Override
     public void onResume() {
         super.onResume();
         run = true;
         mHandler.postDelayed(myRunnable, 1000);
+        setOption();
+        mLocationClient.setLocOption(option);
+        mLocationClient.start();
+        mLocationClient.requestLocation();
     }
 
     @Override
     public void onPause() {
         super.onPause();
         run = false;
+        mLocationClient.stop();
     }
 
     public void updateWifiInfo(WifiInfo wii)
@@ -214,5 +239,33 @@ public class InfoFragment extends android.support.v4.app.Fragment {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
     }
-
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            if (location == null)
+                return ;
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            if (location.getLocType() == BDLocation.TypeGpsLocation){
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation){
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+            }
+            mConnInfo.setText(sb.toString());
+            //mLocationClient.requestLocation();
+        }
+    }
 }
